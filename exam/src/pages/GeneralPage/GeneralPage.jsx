0ac9a347844ab3e030eaad7ef31f868ae7546433
AnnieKhrush <anna.khrushkova.27@gmail.com>
2022-12-29 20:52:37 +0300

    exam

--- exam/src/pages/GeneralPage/GeneralPage.jsx
+++ exam/src/pages/GeneralPage/GeneralPage.jsx
@@ -8,13 +8,18 @@ export function GeneralPage(props) {
 
 
     const [value, setValue] = useState('')
+    const [isOpen, setOpen] = useState(false);
 
     const style = {
         fontSize: '36px'
     }
 
 
-    function translateText() {
+    function ListLanguages() {
+        
+    }
+
+    function selectLang() {
         const options = {
             method: 'GET',
             headers: {
@@ -25,18 +30,40 @@ export function GeneralPage(props) {
         };
         
         fetch('https://microsoft-translator-text.p.rapidapi.com/languages?api-version=3.0', options)
+            .then(response => {response.json(); return(response);})
+            .then(response => console.log(response))
+            .catch(err => {console.error(err); return(err);});
+        
+    }
+
+
+    function translateText() {
+        const options = {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                'X-RapidAPI-Key': 'df5ffa97f3mshfa5277882376ad1p1db7b7jsnb69ba524116a',
+                'X-RapidAPI-Host': 'microsoft-translator-text.p.rapidapi.com'
+            },
+            body: '[{"Text":"I would really like to drive your car around the block a few times."}]'
+        };
+        
+        fetch('https://microsoft-translator-text.p.rapidapi.com/translate?to%5B0%5D=ru&api-version=3.0&profanityAction=NoAction&textType=plain', options)
             .then(response => response.json())
             .then(response => console.log(response))
             .catch(err => console.error(err));
     }
 
+    
 
+    const handleOpen = () => setOpen(true);
 
     return (
         <div className='generalPage'>
             <Header name={'Translate'} />
             <div className='translate_field'>
                 <div className='languages'>
+                
 
                 </div>
                 <div className='form'>
