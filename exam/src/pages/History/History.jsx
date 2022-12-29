8977ebe3a2040bc7120ff5eb32e62becb8b0ac9d
AnnieKhrush <anna.khrushkova.27@gmail.com>
2022-12-29 20:28:02 +0300

    exam

--- exam/src/pages/History/History.jsx
+++ exam/src/pages/History/History.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import './History.scss'
 import { Link } from 'react-router-dom';
+import { Header } from '../../components';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 
 export function History(props) {
-    <div>
-        helo
-    </div>
+
+    const style = {
+        fontSize: '36px'
+    }
+
+    return(
+        <div className='history_page'>
+            <div className='history_head'>
+                <div className='arrow_button'>
+                    <Link to='/' >
+                        <ArrowBackIcon className='arrow' style={ style }/>
+                    </Link>
+                </div>
+                <Header name={'История'}/>
+            </div>
+
+        </div>
+    )
 }
