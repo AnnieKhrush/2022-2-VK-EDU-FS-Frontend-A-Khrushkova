b87c5c76f52f90c28de8703f71b00bd1101b9c00
AnnieKhrush <anna.khrushkova.27@gmail.com>
2022-12-31 15:49:38 +0300

    modified exam

--- exam/src/pages/GeneralPage/GeneralPage.jsx
+++ exam/src/pages/GeneralPage/GeneralPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Header } from '../../components';
 //import { Button } from '../../components';
@@ -8,29 +8,17 @@ import SyncAltIcon from '@mui/icons-material/SyncAlt';
 
 
 
-//function ListLanguages() {
-//    const options = {
-//        method: 'GET',
-//        headers: {
-//            'X-RapidAPI-Key': 'df5ffa97f3mshfa5277882376ad1p1db7b7jsnb69ba524116a',
-//            'X-RapidAPI-Host': 'microsoft-translator-text.p.rapidapi.com',
-//            'x-rapidapi-ua': 'RapidAPI-Playground'
-//        }
-//    };
-//  
-//    fetch('https://microsoft-translator-text.p.rapidapi.com/languages?api-version=3.0', options)
-//        .then(response => response.json())
-//       .then(response => console.log(response))
-//        .catch(err => console.error(err));  
-//  
-//}
-
 export function GeneralPage(props) {
 
 
     const [value, setValue] = useState('');
     const [translation, setTranslation] = useState('');
-//    const [listLang, setListLang] = useState([]);
+    const [listLang, setListLang] = useState([]);
+    const [allInfoLang, setAllInfoLang] = useState({});
+    const [finalLang, setFinalLang] = useState('ru');
+    const [initLang, setInitLang] = useState('');
+    const [selectedLangFrom, setSelectedLangFrom] = useState('');
+    const [selectedLangTo, setSelectedLangTo] = useState('');
     
 
 
@@ -42,7 +30,42 @@ export function GeneralPage(props) {
         fontSize: '16px'
     }
 
-    function translateText(finalLang) {
+
+    function ListLanguages() {
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': 'df5ffa97f3mshfa5277882376ad1p1db7b7jsnb69ba524116a',
+                'X-RapidAPI-Host': 'microsoft-translator-text.p.rapidapi.com',
+                'x-rapidapi-ua': 'RapidAPI-Playground'
+                }
+            };
+    
+            fetch('https://microsoft-translator-text.p.rapidapi.com/languages?api-version=3.0', options)
+                .then(response => response.json())
+                .then(response => {
+                    console.log(response);
+                    let dict = response.dictionary;
+                    setAllInfoLang(dict);
+                    console.log(allInfoLang);
+                    const keys = Object.keys(dict);
+                    let langs = [];
+                    keys.forEach(function(key) {
+                        langs.push(dict[key].name)
+                    });
+                    setListLang(langs);
+                    console.log(listLang);
+                })
+                .catch(err => console.error(err));
+    }
+
+    useEffect(() => {
+        ListLanguages();
+        }, []);
+
+
+
+    function translateText() {
         const options = {
             method: 'POST',
             headers: {
@@ -53,13 +76,13 @@ export function GeneralPage(props) {
             body: `[{"Text":"${value}"}]`
         };
         
-        fetch(`https://microsoft-translator-text.p.rapidapi.com/translate?to%5B0%5D=${finalLang}&api-version=3.0&profanityAction=NoAction&textType=plain`, options)
+        fetch(`https://microsoft-translator-text.p.rapidapi.com/translate?to%5B0%5D=${finalLang}&api-version=3.0&from=${initLang}&profanityAction=NoAction&textType=plain`, options)
             .then(response => response.json())
             .then(response => {
                 console.log(response);
                 setTranslation(response[0].translations[0].text);
                 let newTranslation = {
-                    'from': response[0].detectedLanguage.language,
+                    'from': initLang,
                     'to': response[0].translations[0].to,
                     'initial': value,
                     'final': response[0].translations[0].text,
@@ -78,7 +101,7 @@ export function GeneralPage(props) {
         if (value === '') {
             return;
         } else {
-            translateText('ru');
+            translateText();
         }
         setValue('');
     }
@@ -89,7 +112,66 @@ export function GeneralPage(props) {
     }
 
 
-//    useEffect(() => {ListLanguages();}, [])
+    function handleChangeFromLang(event) {
+        setSelectedLangFrom(event.target.value);
+        //console.log(event.target.value);
+        const keys = Object.keys(allInfoLang);
+        keys.forEach(function(key) {
+            if (allInfoLang[key].name === event.target.value) {
+                setInitLang(key);
+                //console.log(initLang);
+            }
+        });
+
+    }
+
+    function handleChangeToLang(event) {
+        setSelectedLangTo(event.target.value);
+        //console.log(event.target.value);
+        const keys = Object.keys(allInfoLang);
+        keys.forEach(function(key) {
+            if (allInfoLang[key].name === event.target.value) {
+                setFinalLang(key);
+                //console.log(finalLang);
+            }
+        });
+
+    }
+
+
+    function Languages(props) {
+        if (props.class === 'init_lang') {
+            return(
+                <select className={props.class} value={selectedLangFrom} onChange={handleChangeFromLang}>  
+                    {
+                    listLang.map((lang) => {
+                        return(
+                            <option>
+                                {lang}
+                            </option>
+                        )
+                    })
+                    }
+                </select>
+            )
+        } else if (props.class === 'final_lang') {
+            return(
+                <select className={props.class} value={selectedLangTo} onChange={handleChangeToLang}>  
+                    {
+                    listLang.map((lang) => {
+                        return(
+                            <option>
+                                {lang}
+                            </option>
+                        )
+                    })
+                    }
+                </select>
+            )
+        }
+
+        
+    }
 
 
 
@@ -98,9 +180,11 @@ export function GeneralPage(props) {
             <Header name={'Translate'} />
             <div className='translate_field'>
                 <div className='languages'>
+                    <Languages class={'init_lang'} />
                     <div className='switch'>
                         <SyncAltIcon style={arrowStyle} />
                     </div>
+                    <Languages class={'final_lang'} />
                 </div>
                 <div className='form'>
                     <div className='form-input'>
@@ -109,7 +193,9 @@ export function GeneralPage(props) {
                         </form>
                     </div>
                     <div className='form-output'>
-                        {translation}
+                        <div className='output'>
+                            {translation}
+                        </div>
                     </div>
                 </div>
             </div>
@@ -125,14 +211,3 @@ export function GeneralPage(props) {
     )
 }
 
-//<select className='arrow_down'>  
-//{
-//    listLang.map((lang) => {
-//        return(
-//            <option>
-//                {lang.name}
-//            </option>
-//        )
-//    })
-//    }
-//</select>
